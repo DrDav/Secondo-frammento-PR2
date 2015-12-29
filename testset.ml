38e01805acaf44679722a4f21dd2eba2a062f754
Davide R <ifuseekdavider@gmail.com>
2015-12-29 22:11:43 +0100

    Update testset.ml

--- testset.ml
+++ testset.ml
@@ -1,1 +1,8 @@
+let tupexample = Add(Int 23, Add(Bool true, Add(Tuple( Add(Int 45, Add(Int 7, Void)) ), Add(Bool false, Add(Int 12, Void ) ) ) ) );;
+
 let restoper2 = Fun("n", Diff(Var "n", Mul(Div(Var "n", Eint 2), Eint 2)));;
+let true_if_even = Fun("n", ITE( Equ( Appl(restoper2, Var "n"), Eint(0) ), Ebool(true), Ebool(false)));;
+
+evaluate (For("n", tupexample, Appl(true_if_even, Var "n")));;
+
+
