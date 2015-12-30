e592631c9c41ae7a73762f5dcf717b3435ac8b91
Davide R <ifuseekdavider@gmail.com>
2015-12-30 16:05:04 +0100

    Update testset.ml

--- testset.ml
+++ testset.ml
@@ -3,6 +3,7 @@ let tupexample = Add(Int 23, Add(Bool true, Add(Tuple( Add(Int 45, Add(Int 7, Vo
 let restoper2 = Fun("n", Diff(Var "n", Mul(Div(Var "n", Eint 2), Eint 2)));;
 let true_if_even = Fun("n", ITE( Equ( Appl(restoper2, Var "n"), Eint(0) ), Ebool(true), Ebool(false)));;
 
-evaluate (For("n", tupexample, Appl(true_if_even, Var "n")));;
+toList (evaluate (For("n", tupexample, Appl(true_if_even, Var "n"))));;
+toList (evaluate (For("n", tupexample, Appl(restoper2, Var "n"))));;
 
-
+let sommatupla = Let("y", Access(0, Etuple(tupexample)), For("x", tupexample , Plus(Var "x", Var "y")));;
